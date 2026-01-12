161c9a2d000008ebd97ba1a01bb4560e05e7b498
Robson Daniel <robsondaniel2444@gmail.com>
2026-01-12 16:20:56 -0300

    primeiro commit

---
+++ Microservices/Delivery-Traking/src/main/java/com/algaworks/algadelivery/delivery/traking/DeliveryTrakingApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algadelivery.delivery.traking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryTrakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryTrakingApplication.class, args);
+	}
+
+}
